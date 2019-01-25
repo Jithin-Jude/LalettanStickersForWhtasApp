22699d34aafe242f96103f7f82b0f91a37dfb043
Jithin K Jose <jithin.jude68@gmail.com>
2019-01-26 01:34:40 +0530

    intial message

---
+++ app/src/main/java/com/mountzoft/lalettanstickersforwhatsapp/StickerPackListActivity.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.mountzoft.lalettanstickersforwhatsapp;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class StickerPackListActivity extends AddStickerPackActivity implements RewardedVideoAdListener {
+    public static final String EXTRA_STICKER_PACK_LIST_DATA = "sticker_pack_list";
+    private static final int STICKER_PREVIEW_DISPLAY_LIMIT = 5;
+    private LinearLayoutManager packLayoutManager;
+    private RecyclerView packRecyclerView;
+    private StickerPackListAdapter allStickerPacksListAdapter;
+    private WhiteListCheckAsyncTask whiteListCheckAsyncTask;
+    private ArrayList<StickerPack> stickerPackList;
+
+    private RewardedVideoAd mAd;
+    StickerPack currentPack;
+    Boolean reward = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sticker_pack_list);
+        packRecyclerView = findViewById(R.id.sticker_pack_list);
+        stickerPackList = getIntent().getParcelableArrayListExtra(EXTRA_STICKER_PACK_LIST_DATA);
+        showStickerPackList(stickerPackList);
+
+        String adMobAppId = BuildConfig.AD_MOB_APP_ID;
+        MobileAds.initialize(this, adMobAppId);
+        mAd = MobileAds.getRewardedVideoAdInstance(this);
+        mAd.setRewardedVideoAdListener(this);
+        loadRewardedVideoAd();
+    }
+
+    private void loadRewardedVideoAd() {
+        String admobAdUnitId = BuildConfig.ADMOB_AD_UNIT_ID;
+        mAd.loadAd(admobAdUnitId,
+                new AdRequest.Builder().build());
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        loadRewardedVideoAd();
+        if(!reward){
+            Toast.makeText(getApplicationContext(),"You can add this sticker pack to WhatsApp only if you watch the video completely",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+        reward = true;
+        addStickerPackToWhatsApp(currentPack.identifier, currentPack.name);
+        loadRewardedVideoAd();
+    }
+
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+        loadRewardedVideoAd();
+    }
+
+    @Override
+    public void onRewardedVideoCompleted(){
+        loadRewardedVideoAd();
+    }
+
+    public void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(StickerPackListActivity.this, R.style.Theme_AppCompat_Light_Dialog);
+        builder.setTitle("Thanks for your patience");
+        builder.setMessage("Sorry Reward Video not loaded! Please wait for 5 seconds and try again. Only after watching video you can add this sticker pack to WhatsApp");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        whiteListCheckAsyncTask = new WhiteListCheckAsyncTask(this);
+        whiteListCheckAsyncTask.execute(stickerPackList.toArray(new StickerPack[stickerPackList.size()]));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (whiteListCheckAsyncTask != null && !whiteListCheckAsyncTask.isCancelled()) {
+            whiteListCheckAsyncTask.cancel(true);
+        }
+    }
+
+    private void showStickerPackList(List<StickerPack> stickerPackList) {
+        allStickerPacksListAdapter = new StickerPackListAdapter(stickerPackList, onAddButtonClickedListener);
+        packRecyclerView.setAdapter(allStickerPacksListAdapter);
+        packLayoutManager = new LinearLayoutManager(this);
+        packLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(
+                packRecyclerView.getContext(),
+                packLayoutManager.getOrientation()
+        );
+        packRecyclerView.addItemDecoration(dividerItemDecoration);
+        packRecyclerView.setLayoutManager(packLayoutManager);
+        packRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(this::recalculateColumnCount);
+    }
+
+
+    private final StickerPackListAdapter.OnAddButtonClickedListener onAddButtonClickedListener = pack -> {
+        //addStickerPackToWhatsApp(pack.identifier, pack.name);
+        currentPack = pack;
+        reward = false;
+        try {
+            if (mAd.isLoaded()) {
+                mAd.show();
+            }else {
+                loadRewardedVideoAd();
+                showAlertDialog();
+            }
+        } catch (Exception e) {
+            loadRewardedVideoAd();
+            showAlertDialog();
+        }
+    };
+
+    private void recalculateColumnCount() {
+        final int previewSize = getResources().getDimensionPixelSize(R.dimen.sticker_pack_list_item_preview_image_size);
+        int firstVisibleItemPosition = packLayoutManager.findFirstVisibleItemPosition();
+        StickerPackListItemViewHolder viewHolder = (StickerPackListItemViewHolder) packRecyclerView.findViewHolderForAdapterPosition(firstVisibleItemPosition);
+        if (viewHolder != null) {
+            final int max = Math.max(viewHolder.imageRowView.getMeasuredWidth() / previewSize, 1);
+            int numColumns = Math.min(STICKER_PREVIEW_DISPLAY_LIMIT, max);
+            allStickerPacksListAdapter.setMaxNumberOfStickersInARow(numColumns);
+        }
+    }
+
+
+    static class WhiteListCheckAsyncTask extends AsyncTask<StickerPack, Void, List<StickerPack>> {
+        private final WeakReference<StickerPackListActivity> stickerPackListActivityWeakReference;
+
+        WhiteListCheckAsyncTask(StickerPackListActivity stickerPackListActivity) {
+            this.stickerPackListActivityWeakReference = new WeakReference<>(stickerPackListActivity);
+        }
+
+        @Override
+        protected final List<StickerPack> doInBackground(StickerPack... stickerPackArray) {
+            final StickerPackListActivity stickerPackListActivity = stickerPackListActivityWeakReference.get();
+            if (stickerPackListActivity == null) {
+                return Arrays.asList(stickerPackArray);
+            }
+            for (StickerPack stickerPack : stickerPackArray) {
+                stickerPack.setIsWhitelisted(WhitelistCheck.isWhitelisted(stickerPackListActivity, stickerPack.identifier));
+            }
+            return Arrays.asList(stickerPackArray);
+        }
+
+        @Override
+        protected void onPostExecute(List<StickerPack> stickerPackList) {
+            final StickerPackListActivity stickerPackListActivity = stickerPackListActivityWeakReference.get();
+            if (stickerPackListActivity != null) {
+                stickerPackListActivity.allStickerPacksListAdapter.setStickerPackList(stickerPackList);
+                stickerPackListActivity.allStickerPacksListAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+}
